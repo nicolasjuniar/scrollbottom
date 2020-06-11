e3612cf6ac87ad3af0510692257dced416b1b4f7
nicolasjuniar <nicolas@investree.id>
2020-06-11 11:24:25 +0700

    [nico] add class implementation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ScrollBottomFunc from './scrollbottomfunc'
 import ScrollBottomClass from './scrollbottomclass'
 
 function App() {
+  //comment func atau class tergantung kebutuhan(kalo mau nyoba class yg func dicomment dan sebaliknya)
   return (
 		// <ScrollBottomFunc/>
     <ScrollBottomClass/>
